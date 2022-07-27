71c89cad853a21d912d302481582b03b7d483ba8
Yash <yaswanthsaiprabhas123@gmail.com>
2022-07-27 13:04:31 +0530

    first commit

---
+++ calc/views.py
@@ -0,0 +1,13 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(request,'home.html',{'name':'YASH'})
+def add(request):
+    val1=int(request.POST['num1'])
+    val2=int(request.POST['num2'])
+    res=val1+val2
+    return render(request,'result.html',{'result':res})
+
+# Create your views here.
